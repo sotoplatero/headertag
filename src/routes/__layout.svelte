a4fb1b20b19f16e61c357315791b2e93becc457b
Damian Soto <soto.platero@gmail.com>
2021-09-21 06:14:51 -0400

    validate nested rule

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script>import "../app.postcss";</script>
 <slot></slot>
 <footer class="text-center">
-	Proud belong to <a href="//cubablog.net">CubaBlog</a>
+	Proud to belong to <a href="//cubablog.net">CubaBlog</a>
 </footer>
